3d3414ef3419531ab284ae097de25f48e19bc1c9
zabit.zaynukov <zabit.zaynukov@flant.com>
2026-07-02 13:37:29 +0300

    build: upgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.rknchecker"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.rknchecker"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
