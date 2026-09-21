dcd76c578b9e08c461e1dcea6332093703cfc310
zabit.zaynukov <zabit.zaynukov@flant.com>
2026-09-21 18:34:16 +0300

    chore: update bundle identifier prefix to com.zabit and configure test host paths

--- ios/RKNCheckerMac/RKNCheckerMacApp.swift
+++ ios/RKNCheckerMac/RKNCheckerMacApp.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import RKNChecker
 
+#if os(macOS)
 /// Точка входа нативного macOS приложения RKN Checker.
 @main
 struct RKNCheckerMacApp: App {
@@ -13,3 +14,15 @@ struct RKNCheckerMacApp: App {
         .windowStyle(.hiddenTitleBar)
     }
 }
+#else
+/// Фолбэк для сборки под iOS/iPadOS без специфичных для macOS модификаторов окна.
+@main
+struct RKNCheckerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainAdaptiveView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+#endif
